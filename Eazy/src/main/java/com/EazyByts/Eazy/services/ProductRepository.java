981ccb0f8cbb64e171190749625ce0a585e8fce0
Tushar-8489 <131753570+Tushar-8489@users.noreply.github.com>
2024-07-22 22:33:35 +0530

    Delete Eazy directory

--- Eazy/src/main/java/com/EazyByts/Eazy/services/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EazyByts.Eazy.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.EazyByts.Eazy.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
